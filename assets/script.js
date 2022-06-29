2541354d82443322e68536e9b4dc5e2a421fe680
ekukwa <easmsm@gmail.com>
2022-06-28 22:26:43 -0400

    added business hours

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,8 @@
 // Display Current Day at the Top of the Page
 
-/*.var currentDay =  moment(mydate).format('dddd'); 
-
-$('#currentDay').append($('<p>')).html(date.format('dddd'));
-
-
-currentDay = moment(mydate).format('dddd'); {
-    console.log(currentDay); 
-} */
-
-/*var currentDay = (moment()); {
-}*/
-
 currentDay = moment().format('dddd'); {
     console.log(currentDay);
 }
 
 document.getElementById("currentDay").innerHTML = currentDay;
 
-/*$( document ).ready(function() {
-    console.log( "ready!" );
-});
-
-var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-
-$*/
